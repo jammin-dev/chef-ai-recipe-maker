55a5291142d5816d669f4e2da1e3fa817897421b
Ben Jammin <benjamin.vandamme@me.com>
2025-03-25 08:00:50 +0100

    fix: connect redirect to home function with header button

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,9 +4,11 @@ import { TypoLead } from "@/components/ui/typography";
 import { PanelLeftIcon, SquarePen } from "lucide-react";
 import { ModeToggle } from "./ModeToggle";
 import { Button } from "./ui/button";
+import { useNavigateTo } from "@/hooks/use-navigate-to";
 
 const Header = () => {
   const { toggleSidebar } = useSidebar();
+  const { toHome } = useNavigateTo();
 
   return (
     <div className="flex px-5 items-center justify-between mt-5 w-full">
@@ -15,7 +17,7 @@ const Header = () => {
       </Button>
       <Title />
       <div className="flex gap-2">
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" onClick={toHome}>
           <SquarePen size={22} />
         </Button>
         <ModeToggle />
