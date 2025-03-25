cec3ec24def64cead57fc8bbe6304f15dabb494f
Ben Jammin <benjamin.vandamme@me.com>
2025-03-25 08:05:51 +0100

    feat: add i18n language detector

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,18 +1,22 @@
 // src/i18n.ts
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import en from "@/locales/en.json";
 import fr from "@/locales/fr.json";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: en },
-    fr: { translation: fr },
-  },
-  lng: "en", // Default language
-  fallbackLng: "en",
-  interpolation: { escapeValue: false },
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      fr: { translation: fr },
+    },
+    lng: "en", // Default language
+    fallbackLng: "en",
+    interpolation: { escapeValue: false },
+  });
 
 export default i18n;
