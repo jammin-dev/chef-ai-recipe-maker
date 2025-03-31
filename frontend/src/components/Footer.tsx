e52cf0ce2d5f091e047217c57b5db75515eb622d
Ben Jammin <benjamin.vandamme@me.com>
2025-03-31 18:01:20 +0200

    💄 remove "contact" and "report a bug" in app footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,20 +2,20 @@ import { useTranslation } from "react-i18next";
 import { TypoSmall } from "./ui/typography";
 
 const Footer = () => {
-  const { t } = useTranslation();
-  return (
-    <footer className="flex justify-center gap-4 items-center h-10 w-full pt-6 pb-8">
-      <TypoSmall>&copy; 2025 Chef!</TypoSmall>
-      {" | "}
+	const { t } = useTranslation();
+	return (
+		<footer className="flex justify-center gap-4 items-center h-10 w-full p-4">
+			<TypoSmall>&copy; 2025 Chef!</TypoSmall>
+			{/* {" | "}
       <a href="mailto:benjamin.vandamme@me.com" className="ml-4 underline">
         <TypoSmall>{t("contact")}</TypoSmall>
       </a>
       {" | "}
       <a href="mailto:benjamin.vandamme@me.com" className="ml-4 underline">
         <TypoSmall>{t("report a bug")}</TypoSmall>
-      </a>
-    </footer>
-  );
+      </a> */}
+		</footer>
+	);
 };
 
 export default Footer;
